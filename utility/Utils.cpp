7907942db5d915b4027382461aa17d22d9bbdb78
unknown <tom.pouderoux.etu@univ-lemans.fr>
2022-05-15 15:17:15 +0200

    Add GPIO functions like "Turn on LED", "Is JAck removed ?"

--- utility/Utils.cpp
+++ utility/Utils.cpp
@@ -18,3 +18,26 @@ double Utils::clamp(double value, double min, double max) {
         return value;
     }
 }
+
+// Setup basic IO like the LED, the Emergency stop button and the Jack switch
+void Utils::initGPIO(){
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_ARU, INPUT);
+    pinMode(PIN_JACK, INPUT);
+    pullUpDnControl(PIN_JACK, PUD_UP);
+}
+
+// Toggle the status LED on top of the robot
+void Utils::toggleLED(bool state){
+    digitalWrite(PIN_LED, state);
+}
+
+// Check if the ARU is pressed
+bool Utils::isARUPressed(){
+    return !digitalRead(PIN_ARU);
+}
+
+// Check if the Jack is removed
+bool Utils::isJackRemoved(){
+    return digitalRead(PIN_JACK);
+}
